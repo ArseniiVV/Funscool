b64ab06783e4ef057962875c9ddd8f7d9b8eee10
NobleVV <89373000011@yandex.ru>
2025-10-15 08:09:04 +0300

    deploy to gdkids.ru

--- deploy.ps1
+++ deploy.ps1
@@ -3,7 +3,7 @@
 # ==============================
 
 # Configuration
-$BucketName = "school.ufa.funscool.ru"       # your Yandex Cloud bucket name
+$BucketName = "gdkids.ru"       # your Yandex Cloud bucket name
 $BuildDir   = ".output/public"      # static output folder from Nuxt generate
 
 Write-Host "=== 1. Building Nuxt project ==="
