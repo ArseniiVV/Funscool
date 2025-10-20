7405b14fe790fa8ba613be3afdd7ae84edc8767d
NobleVV <89373000011@yandex.ru>
2025-10-20 09:06:41 +0300

    поворот и расстановка в приемуществах

--- components/AdvantageCard.vue
+++ components/AdvantageCard.vue
@@ -23,22 +23,6 @@
           </div>
           <div class="advantages-card__back-text" v-html="description" />
           <div class="advantages-card__back-btn">
-            <!-- <button
-              class="the-button"
-              @click.stop="
-                useModalStore().openLidModal({
-                  title: 'Оставить заявку на ' + title,
-                  text: 'Оставьте заявку и мы свяжемся с вами в ближайшее время',
-                  buttonText: 'Отправить заявку',
-                  group: '',
-                  community: '',
-                  additional: additional,
-                  motive: title && 'Мотив: лагерь ' + title,
-                })
-              "
-            >
-              {{ buttonText }}
-            </button> -->
           </div>
         </div>
       </div>
@@ -99,36 +83,22 @@ function toggleFlip() {
   }, 300);
 }
 
-// Watch external prop
-watch(
-  () => props.isRotated,
-  (newVal) => {
-    if (newVal) toggleFlip();
-  },
-  { immediate: true }
-);
-
 // Run auto-flip only after user sees the card
 onMounted(() => {
-  if (!cardElement.value) return;
+  setTimeout(() => {
+      toggleFlip();
 
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting && !autoFlipped) {
-          autoFlipped = true;
-          const randomDelay = 500 + Math.random() * 500; // 1s + 0–0.5s
-          setTimeout(() => toggleFlip(), randomDelay);
-          observer.disconnect(); // больше не наблюдаем
-        }
-      });
-    },
-    {
-      threshold: 0.3, // карточка считается "видимой", если хотя бы 30% в зоне экрана
-    }
-  );
+      // Далее — бесконечный цикл каждые 7 ± 0–2 сек
+      const flipLoop = () => {
+        const randomDelay = 7000 + Math.random() * 2000; // 7–9 сек
+        setTimeout(() => {
+          toggleFlip();
+          flipLoop();
+        }, randomDelay);
+      };
 
-  observer.observe(cardElement.value);
+      flipLoop();
+    }, 2000);
 });
 </script>
 
