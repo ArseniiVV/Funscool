a53b37ef3a05b164840719585bbe9873282a43ca
NobleVV <89373000011@yandex.ru>
2025-10-20 08:13:42 +0300

    фоны в приемуществах

--- plugins/constants.ts
+++ plugins/constants.ts
@@ -125,7 +125,7 @@ export default defineNuxtPlugin(() => {
     frontImage: '/img/advantages/Advantages4.webp',
     backImage: '/img/ui-elements/Advantages-back4.svg',
     buttonText: 'Подробнее',
-    cardClass: 'beige',
+    cardClass: 'purple',
     additional: 'Забота о здоровье',
     modal: {
       title: 'Узнайте, как мы следим за здоровьем',
@@ -140,7 +140,7 @@ export default defineNuxtPlugin(() => {
     frontImage: '/img/advantages/Advantages5.webp',
     backImage: '/img/ui-elements/Advantages-back5.svg',
     buttonText: 'Подробнее',
-    cardClass: 'purple',
+    cardClass: 'beige',
     additional: 'Билингвальная программа',
     modal: {
       title: 'Узнайте, как мы учим два языка',
