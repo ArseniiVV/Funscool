3795c0578fdb8cbbcbd3661f15078dd3b7b5637a
NobleVV <89373000011@yandex.ru>
2025-11-25 10:06:11 +0200

    advCard & new lid link

--- components/LidModal.vue
+++ components/LidModal.vue
@@ -212,7 +212,7 @@ async function sendRequest() {
   };
 
   try {
-    const response = await fetch('https://et-web.ru/mailing/funscool-lid', {
+    const response = await fetch('https://lids-reciever.funscool.ru/d4e4lbdavl078eclofa5', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(lid),
