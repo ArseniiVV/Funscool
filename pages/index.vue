4bc6d569319d51b61430e48c7561607d7ecd3c15
89373000011@yandex.ru <89373000011@gipervdom.ru>
2026-01-29 22:30:46 +0500

    lazy load is off

--- pages/index.vue
+++ pages/index.vue
@@ -3,50 +3,52 @@
     <HeaderComponent />
     <MainSlider />
 
-    <DeferredClient :loader="() => import('~/components/AdvantagesComponent.vue')" when="visible"
+    <DeferredClient :loader="() => import('~/components/AdvantagesComponent.vue')" when="sequence"
       :placeholder-height="420" />
 
     <DeferredClient :loader="() => import('~/components/PhotoFilm.vue')" when="visible" :placeholder-height="220" />
 
     <!-- Anchor placeholder so /#ageGroupSection works before lazy load -->
     <div id="ageGroupSection" style="scroll-margin-top: 120px"></div>
-    <DeferredClient :loader="() => import('~/components/AgeGroups.vue')" when="visible" :placeholder-height="560" />
+    <DeferredClient :loader="() => import('~/components/AgeGroups.vue')" when="sequence" :placeholder-height="560" />
 
-    <DeferredClient :loader="() => import('~/components/NumbersComponent.vue')" when="visible"
+    <DeferredClient :loader="() => import('~/components/NumbersComponent.vue')" when="sequence"
       :placeholder-height="320" />
 
-    <DeferredClient :loader="() => import('~/components/AdressesComponent.vue')" when="visible"
+    <DeferredClient :loader="() => import('~/components/AdressesComponent.vue')" when="sequence"
       :placeholder-height="460" />
 
     <!-- Anchor placeholder so /#team works before lazy load -->
     <div id="team" style="scroll-margin-top: 120px"></div>
-    <DeferredClient :loader="() => import('~/components/TeamComponent.vue')" when="visible" :placeholder-height="520" />
+    <DeferredClient :loader="() => import('~/components/TeamComponent.vue')" when="sequence"
+      :placeholder-height="520" />
 
-    <DeferredClient :loader="() => import('~/components/FactsSection.vue')" when="visible" :placeholder-height="420" />
+    <DeferredClient :loader="() => import('~/components/FactsSection.vue')" when="sequence" :placeholder-height="420" />
 
-    <DeferredClient :loader="() => import('~/components/JustReading.vue')" when="visible" :placeholder-height="220" />
+    <DeferredClient :loader="() => import('~/components/JustReading.vue')" when="sequence" :placeholder-height="220" />
 
-    <DeferredClient :loader="() => import('~/components/MainReviews.vue')" when="visible" :placeholder-height="520" />
+    <DeferredClient :loader="() => import('~/components/MainReviews.vue')" when="sequence" :placeholder-height="520" />
 
-    <DeferredClient :loader="() => import('~/components/IndividualPath.vue')" when="visible"
+    <DeferredClient :loader="() => import('~/components/IndividualPath.vue')" when="sequence"
       :placeholder-height="420" />
 
-    <DeferredClient :loader="() => import('~/components/FutureComponent.vue')" when="visible"
+    <DeferredClient :loader="() => import('~/components/FutureComponent.vue')" when="sequence"
       :placeholder-height="480" />
 
     <!-- Anchor placeholder so /#news works before lazy load -->
     <div id="news" style="scroll-margin-top: 120px"></div>
-    <DeferredClient :loader="() => import('~/components/NewsComponent.vue')" when="visible" :placeholder-height="520" />
+    <DeferredClient :loader="() => import('~/components/NewsComponent.vue')" when="sequence"
+      :placeholder-height="520" />
 
     <!-- Anchor placeholder so /#contacts works before lazy load -->
     <div id="contacts" style="scroll-margin-top: 120px"></div>
-    <DeferredClient :loader="() => import('~/components/ContactsComponent.vue')" when="visible" />
+    <DeferredClient :loader="() => import('~/components/ContactsComponent.vue')" when="sequence" />
 
-    <DeferredClient :loader="() => import('~/components/MapComponent.vue')" when="visible"/>
+    <DeferredClient :loader="() => import('~/components/MapComponent.vue')" when="visible" />
 
-    <DeferredClient :loader="() => import('~/components/PotentialComponent.vue')" when="visible"/>
+    <DeferredClient :loader="() => import('~/components/PotentialComponent.vue')" when="visible" />
 
-    <DeferredClient :loader="() => import('~/components/FooterComponent.vue')" when="visible" />
+    <DeferredClient :loader="() => import('~/components/FooterComponent.vue')" when="sequence" />
   </div>
 </template>
 
